78e704c55622da1bd5522d7bbfaaaeaf73adba44
Quartz <kosta2005kes@gmail.com>
2026-06-30 12:59:28 +0300

    Spring Boot add in project

---
+++ src/main/java/com/practice/leto/LetoApplication.java
@@ -0,0 +1,13 @@
+package com.practice.leto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetoApplication.class, args);
+	}
+
+}
